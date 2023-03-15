3bf570ec12e35e715c27c04ec204b3b3ca17c1d5
Luis Fernando Rojas González <luisfernando.lzc@hotmail.com>
2023-03-14 21:58:45 -0600

    Grafica y tabla
    Se llenan los datos a través de una API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,66 @@
 console.log("Entro index.js");
+
+(() => {
+    console.log("Entro a la funcion anonima");
+    const BASE_URL = 'https://animales-api.onrender.com';
+    const myChart = document.getElementById('myChart').getContext('2d');
+
+    const loadData = () =>{
+        fetch(BASE_URL + '/animales',
+        {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(data => {
+        for (let i = 0; i < data.data.length; i++) {
+            document.getElementById("tablaLoca").innerHTML +=
+            `<tr>
+                <td>${data.data[i].id}</td>
+                <td>${data.data[i].nombre}</td>
+                <td>${data.data[i].cantidad}</td>
+            </tr>`;
+            console.log(data.data);
+        }
+
+            let labels_for_chart = data.data.map((item) =>{
+                return item.nombre;
+            });
+
+            let data_for_chart = data.data.map((item) =>{
+                return item.cantidad;
+            });
+
+            const grafica = new Chart(myChart, {
+                type: 'bar',
+                data: {
+                    labels: labels_for_chart,
+                    datasets: [{
+                        label: 'Animales del Zoo',
+                        data: data_for_chart,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
+    }
+
+    loadData();
+
+})()
